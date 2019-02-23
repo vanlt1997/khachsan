c4a7bdc4d79d18e809941b3ec4fd84aea9f0e989
dinhcuong2209 <dinhcuong1322@gmail.com>
2019-02-23 19:54:31 +0700

    Change background messagebox

--- resources/views/admin/typeroom/form-create.blade.php
+++ resources/views/admin/typeroom/form-create.blade.php
@@ -251,32 +251,5 @@
     </div>
 
     <script src="{{asset('js/admin/main.js')}}"></script>
-    <script>
-        /*function handleFileSelect(evt) {
-            var files = evt.target.files;
-            for (var i = 0, f; f = files[i]; i++) {
-                if (!f.type.match('image.*')) {
-                    continue;
-                }
-                var reader = new FileReader();
-                reader.onload = (function (theFile) {
-                    return function (e) {
-                        var span = document.createElement('span');
-                        span.innerHTML = ['<img class="img-thumbnail" width=40% height=40% src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
-                        document.getElementById('list').insertBefore(span, null);
-                    };
-                })(f);
-                reader.readAsDataURL(f);
-            }
-        }
-        document.getElementById('files').addEventListener('change', handleFileSelect, false);*/
-        function showImages() {
-            var span = document.createElement('span');
-            span.innerHTML = ['<img class="img-thumbnail" width=40% height=40% src="', e.target.result,
-                '" title="', escape(theFile.name), '"/>'].join('');
-            document.getElementById('list').insertBefore(span, null);
-        }
-    </script>
 @endsection
 
--- resources/views/admin/typeroom/index.blade.php
+++ resources/views/admin/typeroom/index.blade.php
@@ -15,21 +15,17 @@
 @section('content')
     <div class="container">
         @if(Session::has('message'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <div class="alert alert-success alert-dismissible fade show alert-custom-success" role="alert">
+                <i class="fa fa-check"></i>
                 {{Session::get('message')}}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
             </div>
         @endif
-            @if(Session::has('error'))
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    {{Session::get('error')}}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
+        @if(Session::has('error'))
+            <div class="alert alert-danger alert-dismissible fade show alert-custom-error" role="alert">
+                <i class="fa fa-warning"></i>
+                {{Session::get('error')}}
+            </div>
+        @endif
         <table class="table table-striped table-bordered" id="typeroom-table">
             <thead>
             <tr>
