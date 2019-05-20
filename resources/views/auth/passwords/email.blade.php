d8d2bb12acfc7f96aff1a6c535a7280617a30060
levan <levan.gtvt.97@gmail.com>
2019-05-20 22:00:40 +0700

    Push code

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,65 @@
+<?php
+$slidebars = \App\Models\SlideBar::all();
+?>
 @extends('layouts.app')
+@section('title','Login')
+<style>
+    .reservation {
+        margin-top: -613px !important;
+        margin-bottom: 200px !important;
+    }
+</style>
+@section('slidebar')
+    <section class="home-slider owl-carousel">
+        @foreach($slidebars as $slidebar)
+            <div class="slider-item" style="background-image: url({{asset("images/slidebars/$slidebar->url")}});">
+                <div class="overlay"></div>
+            </div>
+        @endforeach
+    </section>
+    <!-- END slider -->
+    <div class="container">
+        <div class="row justify-content-center ftco-animate">
+            <div class="col-lg-6 col-md-6 reservation pt-5 pb-5">
+                <div class="text-danger text-center mb-5"><h3>{{ __('Email For Reset Password') }}</h3></div>
+                <div class="block-17">
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                        <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Send Password Reset Link') }}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
+
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,3 +1,4 @@
+{{--
 @extends('layouts.app')
 
 @section('content')
@@ -62,4 +63,86 @@
         </div>
     </div>
 </div>
+@endsection
+--}}
+<?php
+$slidebars = \App\Models\SlideBar::all();
+?>
+@extends('layouts.app')
+@section('title','Reset password')
+<style>
+    .reservation {
+        margin-top: -613px !important;
+        margin-bottom: 200px !important;
+    }
+</style>
+@section('slidebar')
+    <section class="home-slider owl-carousel">
+        @foreach($slidebars as $slidebar)
+            <div class="slider-item" style="background-image:url({{asset("images/slidebars/$slidebar->url")}});">
+                <div class="overlay"></div>
+            </div>
+        @endforeach
+    </section>
+    <!-- END slider -->
+    <div class="container">
+        <div class="row justify-content-center ftco-animate">
+            <div class="col-lg-6 col-md-6 reservation pt-5 pb-5">
+                <div class="text-danger text-center mb-5"><h3>{{ __('Reset Password') }}</h3></div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
+
+                        <input type="hidden" name="token" value="{{ $token }}">
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Reset Password') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
